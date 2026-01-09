6dbcec9e5cb3b9f5cd837949380aff93bc4e0161
simy46 <missipsa.annane@polymtl.ca>
2026-01-09 13:27:04 -0500

    Refactor main_simulator.py for improved readability and consistency

--- main_simulator.py
+++ main_simulator.py
@@ -115,7 +115,7 @@ def main():
         if mission_manager.show_restart_prompt:
             draw_hud_message(
                 screen,
-                "Mission terminée – Appuyez sur [ESPACE] (ESC = toggle MANUAL/AUTO)",
+                "Mission terminée: Appuyez sur [ESPACE]",
                 position=(80, 30),
                 color=(0, 220, 255),
             )
