6503338be1188c527be52a9fc73710e701951b99
simy46 <missipsa.annane@polymtl.ca>
2026-01-09 18:50:17 -0500

    Update route constants for improved route generation and enhance draw_destination function for better arrow visualization

--- core/route_visualizer.py
+++ core/route_visualizer.py
@@ -12,26 +12,14 @@ def draw_route(world, route, life_time=1.1):
 
 
 def draw_destination(world, location, life_time=1.1):
-    waypoint = world.get_map().get_waypoint(
-        location,
-        project_to_road=True,
-        lane_type=carla.LaneType.Driving,
-    )
-    forward = carla.Vector3D(1.0, 0.0, 0.0)
-    if waypoint is not None:
-        forward = waypoint.transform.get_forward_vector()
-    start = location + carla.Location(z=0.2)
-    end = start + carla.Location(
-        x=forward.x * 2.0,
-        y=forward.y * 2.0,
-        z=0.2,
-    )
+    start = location + carla.Location(z=3.0)
+    end = location + carla.Location(z=0.4)
     world.debug.draw_arrow(
         start,
         end,
-        thickness=0.12,
-        arrow_size=0.35,
-        color=carla.Color(255, 120, 120),
+        thickness=0.14,
+        arrow_size=0.45,
+        color=carla.Color(255, 255, 255),
         life_time=life_time
     )
 
