c4d44a083ed1b42c1b954e55724916f62ac9292c
simy46 <missipsa.annane@polymtl.ca>
2026-01-09 13:39:44 -0500

    Refactor PauseController to streamline pause and resume functionality, removing unnecessary control methods and enhancing vehicle state management.

--- core/pause.py
+++ core/pause.py
@@ -6,6 +6,9 @@ class PauseController:
         self.vehicle = vehicle
         self.telemetry = telemetry
         self.paused = False
+        self._stored_control = None
+        self._stored_velocity = None
+        self._stored_angular_velocity = None
 
     def set_telemetry(self, telemetry):
         self.telemetry = telemetry
@@ -22,7 +25,8 @@ class PauseController:
         self.paused = True
         if self.telemetry is not None:
             self.telemetry.pause()
-        self._apply_pause_control()
+        self._store_vehicle_state()
+        self.vehicle.set_simulate_physics(False)
 
     def resume(self):
         if not self.paused:
@@ -30,28 +34,21 @@ class PauseController:
         self.paused = False
         if self.telemetry is not None:
             self.telemetry.resume()
-        self._release_pause_control()
+        self.vehicle.set_simulate_physics(True)
+        self._restore_vehicle_state()
 
-    def _apply_pause_control(self):
-        self.vehicle.set_target_velocity(carla.Vector3D(0.0, 0.0, 0.0))
-        self.vehicle.set_target_angular_velocity(carla.Vector3D(0.0, 0.0, 0.0))
-        self.vehicle.apply_control(
-            carla.VehicleControl(
-                throttle=0.0,
-                brake=1.0,
-                steer=0.0,
-                hand_brake=True,
-                reverse=False,
-            )
-        )
+    def _store_vehicle_state(self):
+        try:
+            self._stored_control = self.vehicle.get_control()
+        except Exception:
+            self._stored_control = None
+        self._stored_velocity = self.vehicle.get_velocity()
+        self._stored_angular_velocity = self.vehicle.get_angular_velocity()
 
-    def _release_pause_control(self):
-        self.vehicle.apply_control(
-            carla.VehicleControl(
-                throttle=0.0,
-                brake=0.0,
-                steer=0.0,
-                hand_brake=False,
-                reverse=False,
-            )
-        )
+    def _restore_vehicle_state(self):
+        if self._stored_velocity is not None:
+            self.vehicle.set_target_velocity(self._stored_velocity)
+        if self._stored_angular_velocity is not None:
+            self.vehicle.set_target_angular_velocity(self._stored_angular_velocity)
+        if self._stored_control is not None:
+            self.vehicle.apply_control(self._stored_control)
